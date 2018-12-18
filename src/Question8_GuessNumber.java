8ac6c5046bddbeea3d765a18a03fe7eac4050ac9
swethadevi <swetha96devi@gmail.com>
2018-12-18 12:02:56 +0530

    completed 8th Question

--- src/Question8_GuessNumber.java
+++ src/Question8_GuessNumber.java
@@ -6,3 +6,32 @@ Print separate messages for the following:
     c. Number guessed matches the original number
 
  */
+import java.util.Scanner;
+
+public class Question8_GuessNumber
+{
+    public static void main(String args[]) {
+        //Setting the limit of a number to 101
+        int limit = 101;
+        Scanner sc = new Scanner(System.in);
+        int number;
+        do{
+            System.out.println("Guess  a Number \n");
+            number = sc.nextInt();
+            //To check if the number is greater than original number
+            if(number>limit)
+            {
+                System.out.println("Guessed Number is more than Original Number");
+
+            }
+            //condition to check if the number is lesser than the original number
+            else if(number<limit)
+            {
+                System.out.println("Guessed Number is leser than than the Original Number");
+            }
+        }
+        //condition to check if the number is same as the original number to stop loop and exit
+        while (number != limit);
+        System.out.println("Guessed Number matches the Original Number..!!!");
+    }
+}
